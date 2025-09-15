66425e2322652f23a6d8489060e01aa5d22aec83
Deekshith H Y <deekshithhy2005@gmail.com>
2025-09-16 00:03:48 +0530

    Add files via upload

---
+++ basic/B.java
@@ -0,0 +1,18 @@
+class B{
+    public static void main(String[] args) 
+        {
+            System.out.println("m1 starts");
+            B.main();
+            System.out.println("m2 ends");
+        }
+        
+        public static void main(){
+            System.out.println("main starts");
+            System.out.println("main ends");
+        }
+
+
+    }
+
+	
+	
---
+++ basic/C.java
@@ -0,0 +1,12 @@
+public class C{
+    public static void main(String[] args) {
+        System.out.println("execution starts");
+        System.out.println("execution ends");
+
+    }
+        
+    
+}
+   
+    
+
